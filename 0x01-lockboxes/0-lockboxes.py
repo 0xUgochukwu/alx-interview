22360f00d78c990ad783e8c37de00ae67e00582e
0xUgochukwu <0xUgochukwu@gmail.com>
2023-12-07 22:36:45 +0100

    lockboxes

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -9,20 +9,14 @@ def canUnlockAll(boxes):
         Returns True if all the boxs can be unlocked
         and False if they can't
     '''
-    keys = boxes[0]
-    locked_boxes = boxes[1:]
-
-    for n in range(1, len(boxes)):
-        if n in set(keys) and boxes[n] in locked_boxes:
-            keys.extend(boxes[n])
-            locked_boxes.remove(boxes[n])
-
-        for key in set(keys):
-            if boxes[key] in locked_boxes:
-                keys.extend(boxes[key])
-                locked_boxes.remove(boxes[key])
-
-    if locked_boxes:
-        return False
-
-    return True
+    n = len(boxes)
+    seen_boxes = set([0])
+    unseen_boxes = set(boxes[0]).difference(set([0]))
+    while len(unseen_boxes) > 0:
+        boxIdx = unseen_boxes.pop()
+        if not boxIdx or boxIdx >= n or boxIdx < 0:
+            continue
+        if boxIdx not in seen_boxes:
+            unseen_boxes = unseen_boxes.union(boxes[boxIdx])
+            seen_boxes.add(boxIdx)
+    return n == len(seen_boxes)
