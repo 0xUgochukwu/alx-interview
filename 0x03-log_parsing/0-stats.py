da1339b9c385591e6be6da20a4159146886d0a84
0xUgochukwu <0xUgochukwu@gmail.com>
2023-12-21 16:31:50 +0100

    Refactor log parsing script to improve readability and signal handling

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,54 +1,41 @@
 #!/usr/bin/python3
-
 import sys
+import signal
 
 
-def print_msg(dict_sc, total_file_size):
-    """
-    Method to print
-    Args:
-        dict_sc: dict of status codes
-        total_file_size: total of the file
-    Returns:
-        Nothing
-    """
-
-    print("File size: {}".format(total_file_size))
-    for key, val in sorted(dict_sc.items()):
-        if val != 0:
-            print("{}: {}".format(key, val))
+def print_stats():
+    print("File size: {}".format(file_size))
+    for key in sorted(stats.keys()):
+        if stats[key] > 0:
+            print("{}: {}".format(key, stats[key]))
 
 
-total_file_size = 0
-code = 0
-counter = 0
-dict_sc = {"200": 0,
-           "301": 0,
-           "400": 0,
-           "401": 0,
-           "403": 0,
-           "404": 0,
-           "405": 0,
-           "500": 0}
+def signal_handler(sig, frame):
+    print_stats()
+    sys.exit(0)
 
-try:
-    for line in sys.stdin:
-        parsed_line = line.split()  # ✄ trimming
-        parsed_line = parsed_line[::-1]  # inverting
 
-        if len(parsed_line) > 2:
-            counter += 1
+if __name__ == "__main__":
+    signal.signal(signal.SIGINT, signal_handler)
 
-            if counter <= 10:
-                total_file_size += int(parsed_line[0])  # file size
-                code = parsed_line[1]  # status code
+    stats = {"200": 0, "301": 0, "400": 0, "401": 0, "403": 0, "404": 0,
+             "405": 0, "500": 0}
+    file_size = 0
+    count = 0
 
-                if (code in dict_sc.keys()):
-                    dict_sc[code] += 1
-
-            if (counter == 10):
-                print_msg(dict_sc, total_file_size)
-                counter = 0
-
-finally:
-    print_msg(dict_sc, total_file_size)
+    try:
+        for line in sys.stdin:
+            try:
+                parts = line.split()
+                size = int(parts[-1])
+                code = parts[-2]
+                if code in stats:
+                    stats[code] += 1
+                file_size += size
+                count += 1
+                if count % 10 == 0:
+                    print_stats()
+            except ValueError:
+                pass
+    finally:
+        print_stats()
