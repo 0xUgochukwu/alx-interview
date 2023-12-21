bfd338bac433f6f2fb4abfd04308691729c08ab5
0xUgochukwu <0xUgochukwu@gmail.com>
2023-12-21 16:08:29 +0100

    stats

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,54 +1,46 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+'''
+    Log Parsing.
+'''
 
-import sys
-
-
-def print_msg(dict_sc, total_file_size):
-    """
-    Method to print
-    Args:
-        dict_sc: dict of status codes
-        total_file_size: total of the file
-    Returns:
-        Nothing
-    """
-
-    print("File size: {}".format(total_file_size))
-    for key, val in sorted(dict_sc.items()):
-        if val != 0:
-            print("{}: {}".format(key, val))
+import re
+from sys import stdin
 
 
-total_file_size = 0
-code = 0
-counter = 0
-dict_sc = {"200": 0,
-           "301": 0,
-           "400": 0,
-           "401": 0,
-           "403": 0,
-           "404": 0,
-           "405": 0,
-           "500": 0}
+def print_logs_stats(file_size, stats):
+    '''
+        Prints the generated statistics.
+    '''
+    print(f'File size: {file_size}')
+    for code, count in sorted(stats.items()):
+        print(f'{code}: {count}')
 
-try:
-    for line in sys.stdin:
-        parsed_line = line.split()  # ✄ trimming
-        parsed_line = parsed_line[::-1]  # inverting
 
-        if len(parsed_line) > 2:
-            counter += 1
+if __name__ == '__main__':
+    n = 0
+    file_size = 0
+    stats = {}
+    pattern = re.compile(r'''
+        \d+\.\d+\.\d+\.\d+ # ip address
+        \s-\s\[(.*?)\]\s # date
+        "GET\s/projects/260\sHTTP/1.1"\s # request line
+        (?P<status_code>\d+)\s # status code
+        (?P<file_size>\d+) # file size
+    ''', re.VERBOSE)
+    try:
+        for line in stdin:
+            match = pattern.match(line)
+            if match:
+                file_size += int(match.group('file_size'))
+                status_code = match.group('status_code')
+                if status_code in stats:
+                    stats[status_code] += 1
+                else:
+                    stats[status_code] = 1
+            n += 1
+            if n == 10:
+                print_logs_stats(file_size, stats)
+                n = 0
 
-            if counter <= 10:
-                total_file_size += int(parsed_line[0])  # file size
-                code = parsed_line[1]  # status code
-
-                if (code in dict_sc.keys()):
-                    dict_sc[code] += 1
-
-            if (counter == 10):
-                print_msg(dict_sc, total_file_size)
-                counter = 0
-
-finally:
-    print_msg(dict_sc, total_file_size)
+    except KeyboardInterrupt:
+        print_logs_stats(file_size, stats)
