e506427e648d4bed3d45a1fec0ea4524bea0947c
0xUgochukwu <0xUgochukwu@gmail.com>
2024-01-25 15:29:21 +0100

    rot-2D

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -2,14 +2,12 @@
 '''
     ALX Interview - Rotating a 2D Matrix
 '''
-from copy import deepcopy
-
 
 def rotate_2d_matrix(matrix):
     """ Roatate a 2D Matrix
     """
     n = len(matrix)
-    temp = deepcopy(matrix)
+    temp = [[j for j in matrix[i]] for i in range(n)]
     for i in range(n):
         for j in range(len(matrix[0])):
             matrix[i][j] = temp[n - j - 1][i]
