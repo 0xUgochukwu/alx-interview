8009228eb4617bd020598be938eed6dcace4ae0a
0xUgochukwu <0xUgochukwu@gmail.com>
2024-01-30 15:21:57 +0100

    made change

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -2,10 +2,12 @@
 """ ALX Interview Making Change
 """
 
+
 def makeChange(coins, total):
     """
         Given a pile of coins of different values,
-        determine the fewest number of coins needed to meet a given amount total
+        determine the fewest number of coins needed
+        to meet a given amount total
     """
     if total <= 0:
         return 0
